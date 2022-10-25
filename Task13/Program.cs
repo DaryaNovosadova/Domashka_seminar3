1e7f13db1d275fcf97dc298381c4db40e8c69677
Darya <darya.novosadova@gmail.com>
2022-10-25 20:08:36 +0700

    add Task 13 3number

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = number;
+int digitCount = (int)Math.Log10(n) + 1;
+if(digitCount < 3)
+{
+    Console.WriteLine($"{number} - нет третьей цифры");
+}
+else
+{
+    while(number > 1000) 
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine(result);
+}
