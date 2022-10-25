7e6c2ed93d6102a83b3a527ef9750d689f582a9b
Darya <darya.novosadova@gmail.com>
2022-10-25 16:27:39 +0700

    add Task 10 2number

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number3 = number %10;
+int number2 = (number / 10) % 10;
+
+Console.WriteLine(number2);
