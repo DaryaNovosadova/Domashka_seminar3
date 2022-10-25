19074c9c0162c312a2efab1147b34f612028a185
Darya <darya.novosadova@gmail.com>
2022-10-25 17:03:02 +0700

    add Task 15 days

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число дня: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if(number < 6)
+{
+    Console.WriteLine($"Этот день {number} является будним");
+}
+else
+{
+    Console.WriteLine($"Этот день {number} является выходним");
+}
